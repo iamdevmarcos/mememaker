0cd3755e84f696f4446d8a69e86ec30850167c21
Marcos Andre <marcosdev.me@gmail.com>
2022-03-15 07:22:59 -0300

    style: add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
-const App = () => {};
+import React from "react";
+
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+
+      <h2>MemeMaker</h2>
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button,
+  input {
+    outline:0;
+  }
+
+  button {
+    cursor:pointer;
+  }
+`;
