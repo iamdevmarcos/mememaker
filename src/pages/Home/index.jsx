803765705d792d9217c35f5e5ad575b118d7e59c
Marcos Andre <marcosdev.me@gmail.com>
2022-03-15 07:35:43 -0300

    feat: add image banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
-import {} from "./styles";
+import { Wrapper, Card, Templates } from "./styles";
+import logo from "../../images/logo.svg";
 
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Wrapper>
+      <img src={logo} alt="MemeMaker" />
+
+      <Card>
+        <h2>Selecione um template</h2>
+        <Templates>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+        </Templates>
+
+        <h2>Textos</h2>
+      </Card>
+    </Wrapper>
   );
 };
 
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -1,3 +1,47 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  width: 550px;
+  border-radius: 8px;
+  padding: 20px;
+  margin-top: 20px;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+
+  h2 {
+    font-size: 22px;
+    color: #392d2d;
+    margin-bottom: 10px;
+  }
+`;
+
+export const Templates = styled.div`
+  width: 100%;
+  height: 90px;
+  background: #eee;
+  border-radius: 8px;
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+
+  button {
+    border: 0;
+    background: transparent;
+    margin-right: 10px;
+
+    img {
+      width: 53px;
+      height: 53px;
+    }
+  }
+`;
