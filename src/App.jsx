75587d666467abb6cd58a32d549baef0f251c277
Marcos Andre <marcosdev.me@gmail.com>
2022-03-15 07:27:24 -0300

    feat: add home

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { GlobalStyles } from "./styles/GlobalStyles";
 
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <>
       <GlobalStyles />
 
-      <h1>MemeMaker</h1>
+      <Home />
     </>
   );
 };
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {} from "./styles";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
